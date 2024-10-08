9034a2b1fa69c602f38dc883b956e8dd592ce472
arthurmasset2 <sintaxarth@gmail.com>
2024-10-09 00:31:35 +0200

    Optimised code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
 
     // Pipe animation
@@ -15,9 +15,8 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
 
-
     // animations when scrolling
-    window.addEventListener('scroll', function() {
+    window.addEventListener('scroll', function () {
         const animatedBox = document.querySelector('.animated-box');
         const animatedImage = document.querySelector('.animated-image');
 
@@ -41,7 +40,6 @@ document.addEventListener('DOMContentLoaded', function() {
     // Welcome text animation
     const welcomeText = document.getElementById('welcome-text');
     welcomeText.textContent = "Welcome!";  // Set text content
-
 
 
 });
